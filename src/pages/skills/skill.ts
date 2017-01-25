34da0aee0dd63315c9f658e7b0650792e3a386c7
Aswin Kallikadavil <Aswin Kallikadavil>
2017-01-25 23:50:27 +0530

    add Files

---
+++ src/pages/skills/skill.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-skill',
+  templateUrl: 'skill.html'
+})
+export class SkillPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
